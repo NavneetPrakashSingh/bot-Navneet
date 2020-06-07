fe1110c45296ea2c99d256f42211ede07e53a66a
NavneetPrakashSingh <navneet_prakash_singh@live.com>
2020-06-07 16:34:45 -0700

    updating bot to add labels to pr

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
-  // app.on('issues.opened', async context => {
-  //   const issueComment = context.issue({
-  //     body: 'Make sure to include the following:\n' +
-  //       '- [ ] What is the purpose of this PR?\n' +
-  //       '- [ ] Attach the PR with the issue'
-  //   })
-  //   return context.github.issues.createComment(issueComment)
-  // })
-
   app.on('pull_request.opened', async context => {
     app.log('pull request was recieved')
     const issue = context.issue()
